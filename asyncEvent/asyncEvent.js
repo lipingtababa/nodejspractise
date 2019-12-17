3cac008bd26d4f735b5e75b7d892ec848b1b4703
Ma Chi <hellomarch@gmail.com>
2019-12-17 19:55:44 +0000

    Move the close event from server to response

--- asyncEvent/asyncEvent.js
+++ asyncEvent/asyncEvent.js
@@ -4,6 +4,9 @@ server.on("request", function(req, res){
     console.log("request event");
     res.writeHead(200, {"Content-Type":"text/plain"});
     res.end("Hello World\n");
+    res.on("close", function(){
+        console.log("Connection closed");
+    });
 });
 
 server.on("connection", function(){
@@ -13,9 +16,5 @@ server.on("connection", function(){
 server.listen(8124, function(){
     console.log("listening event");
 });
-
-server.on("close", function(){
-    console.log("Connection closed");
-})
 
 console.log("Server starts running");
