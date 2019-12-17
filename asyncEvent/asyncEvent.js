b398eb0dc34266208644ce2b5a1eb61cd851472c
Ma Chi <hellomarch@gmail.com>
2019-12-17 20:39:53 +0100

    implement web server in a more understandable way

---
+++ asyncEvent/asyncEvent.js
@@ -0,0 +1,17 @@
+var http = require("http")
+var server = http.createServer()
+server.on("request", function(req, res){
+    console.log("request event");
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.end("Hello World\n");
+});
+
+server.on("connection", function(){
+    console.log("somebody connected");
+})
+
+server.listen(8124, function(){
+    console.log("listening event");
+});
+
+console.log("Server starts running");
