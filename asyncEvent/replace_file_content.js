2ac35542605f0c1edf10dc47c58eb97e6ca8f13c
Ma Chi <hellomarch@gmail.com>
2019-12-18 17:23:50 +0000

    Use callback functions to handle filesystem manipulation

---
+++ asyncEvent/replace_file_content.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+ 
+var counter = 0;
+var files_counter = 0;
+
+fs. readdir("./data/", function(err, files){
+	if(err)
+	{
+		console.error(err.message);
+	}else{
+		files_counter = files.length
+		files.forEach(function(name){
+			fs.stat('./data/'+name, function(err, stats){
+			if(err){
+				return err;
+			}
+			if(!stats.isFile()){
+				counter++;
+				return;
+			}
+
+			fs.readFile('./data/'+name,'utf8', function (err, data) {
+					if(err){
+						console.error(err.messge);
+					}
+					else{
+						var adjData = data.replace(/apple/g, 'orange');
+						fs.writeFile('./data/'+name, adjData, writeBackHandler);
+					}
+				}
+				);
+				})		
+		});
+	}
+});
+
+
+
+
+function writeBackHandler(err){
+	if(err){
+		console.error(err.message);
+	}
+	else{
+		counter ++;
+		if(counter == files_counter){
+			console.log("All Done")
+		}
+	}
+
+}
