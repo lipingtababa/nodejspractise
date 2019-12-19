b21d6fb24ac9e3f26623fa2dabce5b08fe777f9f
Ma Chi <hellomarch@gmail.com>
2019-12-19 15:51:34 +0000

    Add a file to replace content in multiple files, which just doesn't work

---
+++ asyncEvent/replace_multiple_file_with_async.js
@@ -0,0 +1,55 @@
+var fs = require('fs');
+var async = require('async');
+var dir = './data/'
+
+async.waterfall(
+[
+    function readDir(callback){
+        fs.readdir(dir, function(err, files){
+            callback(err, files);
+        });
+    },
+
+    function loopFiles(files, callback){
+        files.forEach(function(name){
+            callback(null, name);
+        });
+    },
+
+    function checkFile(filename, callback){
+        fs.stat(dir + filename, function(err, stats){
+            callback(err, stats, filename);
+        });
+    },
+
+    function readData(stats, filename, callback){
+        if(stats.isFile()){
+            fs.readFile(dir+ filename, 'utf8', function(err, data){
+	        callback(err, filename, data);
+	    });
+        }
+    },
+    
+    function modify(filename, text, callback){
+        var adjdata = text.replace(/apple/g, 'orange');
+        callback(null, filename, adjdata);
+    },
+    
+    //In fact, you can pass parameters by wrap the system call in abother function
+    function writeBack(filename, text, callback){
+        fs.writeFile(dir+filename+'.new', text, function (err){
+            callback(err, text);
+        })
+    }
+], 
+
+function(err, result){
+    if (err) {
+        console.error(err.message);
+    }
+    else {
+        console.error(result);
+    }
+}
+
+);
