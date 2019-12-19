0b139c3650f6064512d0fae9c3c8a0161f50b322
Ma Chi <hellomarch@gmail.com>
2019-12-19 14:09:42 +0000

    Use async module

---
+++ asyncEvent/replace_file_content_with_async.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+var async = require('async');
+ 
+async.waterfall(
+[
+	function readData(callback){
+		fs.readFile('./data/data1.txt', 'utf8', function(err, data){
+			callback(err, data);
+		});
+	},
+	function modify(text, callback){
+		var adjdata = text.replace(/apple/g, 'orange');
+		callback(null, adjdata);
+	},
+	//TODO as we can see, you can't pass the filename between the functions
+	//And it is hardcoded
+	function writeBack(text, callback){
+		fs.writeFile('./data/data1.txt', text, function (err){
+			callback(err, text);
+		})
+	}
+], 
+
+function(err, result){
+	if (err) {
+		console.error(err.message);
+	}
+	else {
+		console.error(result);
+	}
+}
+
+);
