3135cdd7efda80b4698cf5ad93c173731778c109
Ma Chi <hellomarch@gmail.com>
2019-12-27 21:37:50 +0100

    Add a canvas with circle in it

--- jssite/main.js
+++ jssite/main.js
@@ -1,6 +1,18 @@
 $(document).ready(
 	function(){
 		'use strict';
-		console.log("main.jsloaded");
+		paper.install(window)
+
+		//Draw a green circle in first canvas
+		paper.setup(document.getElementById('maincanvas'));
+		var tool = new Tool();
+		tool.onMouseDown = function(event){
+			var c = Shape.Circle(event.point, 20);
+			c.fillColor = 'green'
+		}
+
+		paper.view.draw();
+		console.log("main.js has been loaded");
+
 	}
 );
